9501ce3acd9cbe41a71e1c61d664a7a2a0ec31b0
huanghao-code <huanghao_bupt@sina.com>
2017-10-05 23:21:59 -0400

    Create string_trim.cpp

---
+++ string_trim.cpp
@@ -0,0 +1,11 @@
+string trim(const string& str){
+	const auto pos1 = str.find_first_not_of(' ');
+	if(pos1 == string::npos){
+		return "";
+	}else{
+		const auto pos2 = str.find_last_not_of(' ');
+		const auto len = pos2 - pos1 + 1;
+
+		return str.substr(pos1, len);
+	}
+}
